2e956f91f4ad22049594d48fa164ccce1596bfb3
jakob <jakob@DESKTOP-BEG0Q6G>
2021-09-04 12:05:37 +0200

    Add project files.

---
+++ Geometri_take_2/Logic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri_take_2
+{
+    public class Logic
+    {
+        private int side_A;
+
+        public int Side_A
+        {
+            get { return side_A; }
+            set { side_A = value; }
+        }
+        public int Perimeter()
+        {
+            return side_A * 4;
+        }
+        public int area()
+        {
+            return side_A * side_A;
+        }
+        public Logic()
+        {
+
+        }
+    }
+}
---
+++ Geometri_take_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Geometri_take_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic Square1 = new Logic();
+            Square1.Side_A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine(Square1.Perimeter());
+            Console.WriteLine();
+            Console.WriteLine(Square1.area());
+        }
+
+    }
+}
